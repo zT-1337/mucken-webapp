22710ceab78088169e4743a62e2c3a442fbfa071
Thomas Zerr <thomas.zerr@cloudogu.com>
2025-10-18 22:55:02 +0200

    Move header to _auth.index route

--- src/main/client/src/routes/__root.tsx
+++ src/main/client/src/routes/__root.tsx
@@ -8,7 +8,6 @@ function RootComponent() {
   return (
     <div className={"h-screen pt-4"}>
       <main className={"container mx-auto border-2 p-4 shadow bg-background"}>
-        <h1 className={"text-4xl text-center my-4"}>Auf einen Muck?</h1>
         <Outlet />
       </main>
     </div>
